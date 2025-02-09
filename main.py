7ccfba11c5b9440d28d576dfdf0cb186087fc0e9
VK88 <126750132+VilleK88@users.noreply.github.com>
2025-02-09 16:25:51 +0200

    Looppi korjattu. Nyt lentoasemalta toiselle siirtyminen onnistuu myös update_loopissa.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from geopy.distance import geodesic
 from datetime import datetime, timedelta
 from db import connect_db
 done = False
-update_distance = 0
+distance_traveled = 0
 current_distance = 0
 speed_kmh = 780
 fuel_capacity = 25941
@@ -26,16 +26,18 @@ def calculate_distance(icao1, icao2):
     koord2 = icao2[1], icao2[2]
     return geodesic(koord1, koord2).kilometers
 def update_loop():
-    global update_distance, current_fuel, done
+    global distance_traveled, current_fuel, done
     current_time = datetime.now()
-    while update_distance > 0 and not done:
+    while distance_traveled > 0:
         time.sleep(1)
-        update_distance -= (speed_kmh * time_multiplier / 3600)
+        distance_traveled -= (speed_kmh * time_multiplier / 3600)
+        if distance_traveled < 0:
+            distance_traveled = 0
         current_fuel -= (fuel_per_km * (speed_kmh * time_multiplier / 3600))
         current_time += timedelta(seconds=time_multiplier)
-        print(f"distance: {update_distance:.2f} km, time: {current_time.strftime("%H:%M")} fuel: {current_fuel:.2f}")
+        print(f"distance: {distance_traveled:.2f} km, time: {current_time.strftime("%H:%M")} fuel: {current_fuel:.2f}")
 def start():
-    global update_distance, current_fuel
+    global distance_traveled, current_fuel
     current_fuel = fuel_capacity
     icao1 = input("1. ICAO-koodi: ").strip().upper()
     icao2 = input("2. ICAO-koodi: ").strip().upper()
@@ -43,14 +45,15 @@ def start():
     koord2 = get_airport(icao2)
     if not koord1 or not koord2:
         return
-    update_distance = calculate_distance(koord1, koord2)
-    print(f"Lentoaseman {koord1[0]} {icao1} etäisyys {koord2[0]} {icao2} on {update_distance:.2f} kilometriä")
+    distance_traveled = calculate_distance(koord1, koord2)
+    print(f"Lentoaseman {koord1[0]} {icao1} etäisyys {koord2[0]} {icao2} on {distance_traveled:.2f} kilometriä")
     t1 = threading.Thread(target=update_loop, daemon=True)
     t1.start()
     t1.join()
     print(f"Saavuit {koord2[0]} {icao2}")
     return icao2, koord2[0]
 def main_program():
+    global distance_traveled
     current_icao, current_name = start()
     while True:
         icao = input("2. ICAO-koodi: ").strip().upper()
@@ -58,9 +61,10 @@ def main_program():
         koord2 = get_airport(icao)
         if not koord1 or not koord2:
             return
-        update_distance = calculate_distance(koord1, koord2)
-        print(f"Update distance: {update_distance}")
-        print(f"Lentoaseman {koord1[0]} {current_icao} etäisyys {koord2[0]} {icao} on {update_distance:.2f} kilometriä")
+        distance_traveled = None
+        distance_traveled = calculate_distance(koord1, koord2)
+        print(f"Koord1: {koord1} ja Koord2: {koord2}")
+        print(f"Lentoaseman {koord1[0]} {current_icao} etäisyys {koord2[0]} {icao} on {distance_traveled:.2f} kilometriä")
         t1 = threading.Thread(target=update_loop, daemon=True)
         t1.start()
         t1.join()

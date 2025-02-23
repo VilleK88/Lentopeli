3f3be00d4cf7c7d09c255f0bad68acf36d55ef2f
VK88 <126750132+VilleK88@users.noreply.github.com>
2025-02-23 17:37:09 +0200

    Bugi korjaus. Nyt saavuit lentokentälle teksti tulee vain jos remaining_distance <= 0.

--- main.py
+++ main.py
@@ -73,8 +73,9 @@ def main_program():
     while True:
         on_flight = False
         server.update_server(flight.new_lat, flight.new_lon, False)
-        draw_arrived_airport(current_icao[0], current_icao[1], screen, 20, 50, font)
-        time.sleep(2)
+        if remaining_distance <= 0:
+            draw_arrived_airport(current_icao[0], current_icao[1], screen, 20, 50, font)
+            time.sleep(2)
 
         """ tänne huolto koodi """
 

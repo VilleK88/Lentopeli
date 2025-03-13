321e334f2cc4de0ed5badbb69a519bfae07b3fb0
VK88 <126750132+VilleK88@users.noreply.github.com>
2025-03-13 12:24:49 +0200

    Käyttämättömäksi jäänyt kirjasto poistettu main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Loops import flight, user
 from Routes import server
 from datetime import datetime
-from Utils.utils import get_valid_icao, draw_arrived_airport, initialize_pygame_screen
+from Utils.utils import get_valid_icao, initialize_pygame_screen
 from Database.db import get_columns_and_tables, get_airport_coords
 
 time_multiplier = 150 # tämä muuttuja määrittää pelin nopeuden

4351e13da861b9feb8436797eaac782667a982cf
VK88 <126750132+VilleK88@users.noreply.github.com>
2025-03-20 16:31:41 +0200

    weather.py-tiedosto poistettu ja kaikki sitä koskevat kirjasto-haut, koska sitä ei enää tarvita. Weather-pyynnöt hoidetaan jatkossa server.py kautta.

--- Loops/user.py
+++ Loops/user.py
@@ -1,14 +1,9 @@
 import main
-from Utils.utils import draw_user_list, draw_text, press_button_list, wipe_pygame_screen, \
-    update_pygame_screen, draw_centered_list, draw_text_to_center_x
-from Utils.weather import get_weather
-import pygame
-from Database.db import show_current_users, get_users_and_set_as_logged_in, check_if_name_in_db, add_user_to_db, get_logged_in_user_data, \
-    save_game_progress, get_inventory, log_out, get_airport_coords
+from Utils.utils import draw_text
+from Database.db import get_users_and_set_as_logged_in, check_if_name_in_db, add_user_to_db, get_logged_in_user_data, \
+    get_inventory, get_airport_coords
 import time
-import sys
-from Loops import flight, shop, customers
-import requests
+from Loops import flight
 
 # Käyttäjän tiedot (globaalit muuttujat)
 user_name = ""
